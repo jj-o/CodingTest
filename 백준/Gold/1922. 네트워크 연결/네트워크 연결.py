56211a5972c31548558079d9bbf280dac30d2dc0
jj-o <kmjkyj3086@gmail.com>
2024-09-17 13:23:26 +0900

    [Gold IV] Title: 네트워크 연결, Time: 2624 ms, Memory: 46628 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,37 @@
+
+def find_parent(parent,x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n = int(input())
+m = int(input())
+
+edges = []
+result = 0
+parent = [0] * (n+1)
+
+for i in range(1,n+1):
+    parent[i]=i
+
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    edges.append((c,a,b))
+
+edges.sort() # 비용 순으로 정렬
+
+for edge in edges:
+    cost,a,b = edge
+    if find_parent(parent,a) != find_parent(parent,b):
+        union_parent(parent,a,b)
+        result += cost
+
+print(result)
